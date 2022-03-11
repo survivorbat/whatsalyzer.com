534eaf0477e98e32c899e34e84e9bf0316e2d8a2
survivorbat <djbatcat@gmail.com>
2022-03-11 17:00:45 +0100

    Updates

---
+++ precache-manifest.a65bf14036fbfe21a2c716fa40fae35f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30106455443c521406fc",
+    "url": "/whatsalyzer/static/css/main.48780860.chunk.css"
+  },
+  {
+    "revision": "30106455443c521406fc",
+    "url": "/whatsalyzer/static/js/main.bf2afec0.chunk.js"
+  },
+  {
+    "revision": "7b263d6db523bbb5df41",
+    "url": "/whatsalyzer/static/js/runtime~main.ea3df361.js"
+  },
+  {
+    "revision": "d3621b16132b610d8e27",
+    "url": "/whatsalyzer/static/css/2.1ff2da24.chunk.css"
+  },
+  {
+    "revision": "d3621b16132b610d8e27",
+    "url": "/whatsalyzer/static/js/2.064809ac.chunk.js"
+  },
+  {
+    "revision": "a98449a99e5beb98e9d45184c2c9559f",
+    "url": "/whatsalyzer/static/media/howto-1.a98449a9.jpeg"
+  },
+  {
+    "revision": "5d3794be69a6ddab315d0a2f0027a069",
+    "url": "/whatsalyzer/index.html"
+  }
+];
